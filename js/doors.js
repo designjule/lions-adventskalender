6ee5009083aaab6a0f792ee2029b6a7fbb5d05f8
Adrian Fiedler <adrian.fiedler@capgemini.com>
2021-12-08 15:56:05 +0100

    added 13-16

--- js/doors.js
+++ js/doors.js
@@ -3,7 +3,48 @@ const doors = [
     row: 0,
     col: 0,
     number: "15",
-    prices: [],
+    prices: [
+      {
+        sponsor: "Ristorante-Pizzeria Da Mimmo, Weil der Stadt",
+        text: "1 Gutschein für 2 Pizzen + 2 Getränke",
+        winner: "K1890",
+      },
+      {
+        sponsor: "Ristorante Pizzeria Da Nono",
+        text: "1 Gutschein für 2 Pizzen + 2 Getränke",
+        winner: "K0480",
+      },
+      {
+        sponsor: "Sanitätshaus Fünfer, Weil der Stadt",
+        text: "1 Einkaufsgutschein im Wert von 20,- €",
+        winner: "K1656",
+      },
+      {
+        sponsor: "Sanitätshaus Fünfer, Weil der Stadt",
+        text: "1 Einkaufsgutschein im Wert von 20,- €",
+        winner: "K1147",
+      },
+      {
+        sponsor: "KEVIN KUGEL® CHOCOLATIER, Sindelfingen",
+        text: "1 Gutschein für 1 Pralinen-Workshop für 1 Person",
+        winner: "K0278",
+      },
+      {
+        sponsor: "Diefenbach Bäckerei & Konditorei, Weil der Stadt",
+        text: "1 Backwaren-Gutschein im Wert von 25,- €",
+        winner: "K0264",
+      },
+      {
+        sponsor: "Diefenbach Bäckerei & Konditorei, Weil der Stadt",
+        text: "1 Backwaren-Gutschein im Wert von 25,- €",
+        winner: "K1956",
+      },
+      {
+        sponsor: "Diefenbach Bäckerei & Konditorei, Weil der Stadt",
+        text: "1 Backwaren-Gutschein im Wert von 25,- €",
+        winner: "K0176",
+      },
+    ],
   },
   {
     row: 0,
@@ -46,7 +87,43 @@ const doors = [
     row: 0,
     col: 2,
     number: "16",
-    prices: [],
+    prices: [
+      {
+        sponsor: "BOPPEL MEDICAL ENGINEERING-Schlafdiagnose, Weil der Stadt",
+        text: "1 Gutschein für eine ambulante Schlafuntersuchung Standard mit Auswertung im Wert von 59,- €",
+        winner: "K1406",
+      },
+      {
+        sponsor: "BOPPEL MEDICAL ENGINEERING-Schlafdiagnose, Weil der Stadt",
+        text: "1 Gutschein für eine ambulante Schlafuntersuchung Standard mit Auswertung im Wert von 59,- €",
+        winner: "K1614",
+      },
+      {
+        sponsor: "KEVIN KUGEL® CHOCOLATIER, Sindelfingen",
+        text: "1 Einkaufsgutschein im Wert von 25,- €",
+        winner: "K0029",
+      },
+      {
+        sponsor: "KEVIN KUGEL® CHOCOLATIER, Sindelfingen",
+        text: "1 Einkaufsgutschein im Wert von 25,- €",
+        winner: "K0587",
+      },
+      {
+        sponsor: "KEVIN KUGEL® CHOCOLATIER, Sindelfingen",
+        text: "1 Einkaufsgutschein im Wert von 25,- €",
+        winner: "K1884",
+      },
+      {
+        sponsor: "KEVIN KUGEL® CHOCOLATIER, Sindelfingen",
+        text: "1 Einkaufsgutschein im Wert von 25,- €",
+        winner: "K1705",
+      },
+      {
+        sponsor: "KEVIN KUGEL® CHOCOLATIER, Sindelfingen",
+        text: "1 Einkaufsgutschein im Wert von 25,- €",
+        winner: "K0413",
+      },
+    ],
   },
   {
     row: 0,
@@ -89,26 +166,22 @@ const doors = [
     prices: [
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text:
-          "2 Bücher<ul><li>Notfall-Apotheke in und aus der Natur</li><li>Die 12 wichtigsten essbaren Wildpflanzen</li></ul>im Gesamtwert von 22,- €",
+        text: "2 Bücher<ul><li>Notfall-Apotheke in und aus der Natur</li><li>Die 12 wichtigsten essbaren Wildpflanzen</li></ul>im Gesamtwert von 22,- €",
         winner: "K1105",
       },
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text:
-          "2 Kochbücher<ul><li>Knuspermüsli & Granola</li><li>Säfte & Smoothies</li></ul>im Gesamtwert von 25,90 €",
+        text: "2 Kochbücher<ul><li>Knuspermüsli & Granola</li><li>Säfte & Smoothies</li></ul>im Gesamtwert von 25,90 €",
         winner: "K1090",
       },
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text:
-          "2 Kochbücher<ul><li>Heiße Schokolade: Die besten Rezepte</li><li>Crumbles: süß und pikant</li></ul>im Gesamtwert von 25,90 €",
+        text: "2 Kochbücher<ul><li>Heiße Schokolade: Die besten Rezepte</li><li>Crumbles: süß und pikant</li></ul>im Gesamtwert von 25,90 €",
         winner: "K1051",
       },
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text:
-          "2 Bücher<ul><li>Veganes Fest: Rezepte für Advent, Weihnachten und Silvester</li><li>Fingerfood vegan & vollwertig</li></ul>im Gesamtwert von 25,75 €",
+        text: "2 Bücher<ul><li>Veganes Fest: Rezepte für Advent, Weihnachten und Silvester</li><li>Fingerfood vegan & vollwertig</li></ul>im Gesamtwert von 25,75 €",
         winner: "K0478",
       },
       {
@@ -204,50 +277,42 @@ const doors = [
       },
       {
         sponsor: "Mosterei Franz Decker, Weil der Stadt",
-        text:
-          "1 Gutschein für 3 Pack Weil der Städter Apfelsaft (5 Liter Bag in Box)",
+        text: "1 Gutschein für 3 Pack Weil der Städter Apfelsaft (5 Liter Bag in Box)",
         winner: "K0231",
       },
       {
         sponsor: "Mosterei Franz Decker, Weil der Stadt",
-        text:
-          "1 Gutschein für 3 Pack Weil der Städter Apfelsaft (5 Liter Bag in Box)",
+        text: "1 Gutschein für 3 Pack Weil der Städter Apfelsaft (5 Liter Bag in Box)",
         winner: "K1855",
       },
       {
         sponsor: "Mosterei Franz Decker, Weil der Stadt",
-        text:
-          "1 Gutschein für 3 Pack Weil der Städter Apfelsaft (5 Liter Bag in Box)",
+        text: "1 Gutschein für 3 Pack Weil der Städter Apfelsaft (5 Liter Bag in Box)",
         winner: "K1134",
       },
       {
         sponsor: "Mosterei Franz Decker, Weil der Stadt",
-        text:
-          "1 Gutschein für 3 Pack Weil der Städter Apfelsaft (5 Liter Bag in Box)",
+        text: "1 Gutschein für 3 Pack Weil der Städter Apfelsaft (5 Liter Bag in Box)",
         winner: "K1115",
       },
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text:
-          "2 Kochbücher <ul><li>Pizza originale</li><li>Mozzarella originale</li></ul> im Gesamtwert von 34,95 €",
+        text: "2 Kochbücher <ul><li>Pizza originale</li><li>Mozzarella originale</li></ul> im Gesamtwert von 34,95 €",
         winner: "K0217",
       },
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text:
-          "2 Bücher<ul><li>Meze: Orientalische Gaumengeschenke</li><li>Santorin: Rezepte & Bilder aus der Ägäis</li></ul>im Gesamtwert von 31,95 €",
+        text: "2 Bücher<ul><li>Meze: Orientalische Gaumengeschenke</li><li>Santorin: Rezepte & Bilder aus der Ägäis</li></ul>im Gesamtwert von 31,95 €",
         winner: "K1712",
       },
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text:
-          "2 Bücher<ul><li>Das Männerkochbuch</li><li>Das Männerkochbuch – Crossover</li></ul>Im Gesamtwert von 41,90  €",
+        text: "2 Bücher<ul><li>Das Männerkochbuch</li><li>Das Männerkochbuch – Crossover</li></ul>Im Gesamtwert von 41,90  €",
         winner: "K0199",
       },
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text:
-          "2 Bücher<ul><li>Die Sprache der Küche</li><li>Der kleine Restaurantkritiker, Notizbuch</li></ul>Im Gesamtwert von 34,90  €",
+        text: "2 Bücher<ul><li>Die Sprache der Küche</li><li>Der kleine Restaurantkritiker, Notizbuch</li></ul>Im Gesamtwert von 34,90  €",
         winner: "K0952",
       },
       {
@@ -413,7 +478,28 @@ const doors = [
     row: 3,
     col: 1,
     number: "14",
-    prices: [],
+    prices: [
+      {
+        sponsor: "Versicherungsbüro Jürgen Roeßle, Weil der Stadt - Merklingen",
+        text: "1 x Weiler Taler im Wert von 20,- €",
+        winner: "K1670",
+      },
+      {
+        sponsor: "CORRADO PIZZA IMBISS, Weil der Stadt",
+        text: "1 Verzehrgutschein im Wert von 25,- €",
+        winner: "K1174",
+      },
+      {
+        sponsor: "KEVIN KUGEL® CHOCOLATIER, Sindelfingen",
+        text: "1 Gutschein für 1 Pralinen-Workshop für 1 Person",
+        winner: "K0767",
+      },
+      {
+        sponsor: "KEVIN KUGEL® CHOCOLATIER, Sindelfingen",
+        text: "1 Gutschein für 1 Pralinen-Workshop für 1 Person",
+        winner: "K0234",
+      },
+    ],
   },
   {
     row: 3,
@@ -428,26 +514,22 @@ const doors = [
     prices: [
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text:
-          "Bodenseelust: Eine kulinarische Reise mit Rezepten der Region im Wert von 32,- €",
+        text: "Bodenseelust: Eine kulinarische Reise mit Rezepten der Region im Wert von 32,- €",
         winner: "K0396",
       },
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text:
-          "2 Bücher<ul><li>Die 12 wichtigsten essbaren Wildpflanzen: Bestimmen, sammeln und zubereiten</li><li>Die 12 besten essbaren Pionierpflanzen: Bestimmen, sammeln und zubereiten</li></ul> inkl. einem Sammelbeutel im Wert von 24,80 €",
+        text: "2 Bücher<ul><li>Die 12 wichtigsten essbaren Wildpflanzen: Bestimmen, sammeln und zubereiten</li><li>Die 12 besten essbaren Pionierpflanzen: Bestimmen, sammeln und zubereiten</li></ul> inkl. einem Sammelbeutel im Wert von 24,80 €",
         winner: "K1404",
       },
       {
         sponsor: "SZ/BZ - Sindelfinger Zeitung/Böblinger Zeitung",
-        text:
-          "1 Gutschein für ein Halbjahres-Abo für das  SZ/BZ-E-Paper im Wert von 137,- €",
+        text: "1 Gutschein für ein Halbjahres-Abo für das  SZ/BZ-E-Paper im Wert von 137,- €",
         winner: "K1437",
       },
       {
         sponsor: "SZ/BZ - Sindelfinger Zeitung/Böblinger Zeitung",
-        text:
-          "1 Gutschein für ein Halbjahres-Abo für das  SZ/BZ-E-Paper im Wert von 137,- €",
+        text: "1 Gutschein für ein Halbjahres-Abo für das  SZ/BZ-E-Paper im Wert von 137,- €",
         winner: "K0128",
       },
     ],
@@ -465,14 +547,12 @@ const doors = [
     prices: [
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text:
-          "2 Bücher<ul><li>Trüffeltang, Ananas-Ameisen und die Fässer des Herrn Takahashi</li><li>Der kleine Restaurantkritiker, Notizbuch</li></ul> im Gesamtwert von 35,- €",
+        text: "2 Bücher<ul><li>Trüffeltang, Ananas-Ameisen und die Fässer des Herrn Takahashi</li><li>Der kleine Restaurantkritiker, Notizbuch</li></ul> im Gesamtwert von 35,- €",
         winner: "K0789",
       },
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text:
-          "2 Kochbücher<ul><li>Brotaufstriche vegan & vollwertig</li><li>Suppen, Aufläufe und Eintöpfe vegan & vollwertig</li></ul> im Gesamtwert von 25,60 €",
+        text: "2 Kochbücher<ul><li>Brotaufstriche vegan & vollwertig</li><li>Suppen, Aufläufe und Eintöpfe vegan & vollwertig</li></ul> im Gesamtwert von 25,60 €",
         winner: "K0031",
       },
       {
@@ -544,8 +624,7 @@ const doors = [
       },
       {
         sponsor: "INNENARCHITEKTIN JACQUELINE MÖRK, Weil der Stadt",
-        text:
-          "1 Gutschein für eine Einrichtungberatung oder Home Staging im Wert von 700,-Euro",
+        text: "1 Gutschein für eine Einrichtungberatung oder Home Staging im Wert von 700,-Euro",
         winner: "K1307",
       },
     ],
@@ -611,6 +690,42 @@ const doors = [
     row: 5,
     col: 3,
     number: "13",
-    prices: [],
+    prices: [
+      {
+        sponsor: "Mitglieder des Lions Club Johannes Kepler",
+        text: "1 x Weiler Taler im Wert von 20,- €",
+        winner: "K0485",
+      },
+      {
+        sponsor: "Mitglieder des Lions Club Johannes Kepler",
+        text: "1 x Weiler Taler im Wert von 20,- €",
+        winner: "K1793",
+      },
+      {
+        sponsor: "Mitglieder des Lions Club Johannes Kepler",
+        text: "1 x Weiler Taler im Wert von 20,- €",
+        winner: "K1182",
+      },
+      {
+        sponsor: "Mitglieder des Lions Club Johannes Kepler",
+        text: "1 x Weiler Taler im Wert von 20,- €",
+        winner: "K1713",
+      },
+      {
+        sponsor: "Mitglieder des Lions Club Johannes Kepler",
+        text: "1 x Weiler Taler im Wert von 20,- €",
+        winner: "K0337",
+      },
+      {
+        sponsor: "Presswerk Süd · Rotomedia, Weil der Stadt - Merklingen",
+        text: "100 x CD Rohlinge farbig bedruckt im Wert von 100,- € ",
+        winner: "K1440",
+      },
+      {
+        sponsor: "Parkrestaurant Stadthalle Sindelfingen",
+        text: "1 Verzehrgutschein Im Wert von 50,- €",
+        winner: "K1864",
+      },
+    ],
   },
 ];
