44a3f89c924104202ae6315df52915fc82413f22
Adrian <adrian_fiedler@msn.com>
2021-12-12 21:36:15 +0100

    Added 17

--- js/doors.js
+++ js/doors.js
@@ -90,12 +90,14 @@ const doors = [
     prices: [
       {
         sponsor: "BOPPEL MEDICAL ENGINEERING-Schlafdiagnose, Weil der Stadt",
-        text: "1 Gutschein für eine ambulante Schlafuntersuchung Standard mit Auswertung im Wert von 59,- €",
+        text:
+          "1 Gutschein für eine ambulante Schlafuntersuchung Standard mit Auswertung im Wert von 59,- €",
         winner: "K1406",
       },
       {
         sponsor: "BOPPEL MEDICAL ENGINEERING-Schlafdiagnose, Weil der Stadt",
-        text: "1 Gutschein für eine ambulante Schlafuntersuchung Standard mit Auswertung im Wert von 59,- €",
+        text:
+          "1 Gutschein für eine ambulante Schlafuntersuchung Standard mit Auswertung im Wert von 59,- €",
         winner: "K1614",
       },
       {
@@ -166,22 +168,26 @@ const doors = [
     prices: [
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text: "2 Bücher<ul><li>Notfall-Apotheke in und aus der Natur</li><li>Die 12 wichtigsten essbaren Wildpflanzen</li></ul>im Gesamtwert von 22,- €",
+        text:
+          "2 Bücher<ul><li>Notfall-Apotheke in und aus der Natur</li><li>Die 12 wichtigsten essbaren Wildpflanzen</li></ul>im Gesamtwert von 22,- €",
         winner: "K1105",
       },
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text: "2 Kochbücher<ul><li>Knuspermüsli & Granola</li><li>Säfte & Smoothies</li></ul>im Gesamtwert von 25,90 €",
+        text:
+          "2 Kochbücher<ul><li>Knuspermüsli & Granola</li><li>Säfte & Smoothies</li></ul>im Gesamtwert von 25,90 €",
         winner: "K1090",
       },
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text: "2 Kochbücher<ul><li>Heiße Schokolade: Die besten Rezepte</li><li>Crumbles: süß und pikant</li></ul>im Gesamtwert von 25,90 €",
+        text:
+          "2 Kochbücher<ul><li>Heiße Schokolade: Die besten Rezepte</li><li>Crumbles: süß und pikant</li></ul>im Gesamtwert von 25,90 €",
         winner: "K1051",
       },
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text: "2 Bücher<ul><li>Veganes Fest: Rezepte für Advent, Weihnachten und Silvester</li><li>Fingerfood vegan & vollwertig</li></ul>im Gesamtwert von 25,75 €",
+        text:
+          "2 Bücher<ul><li>Veganes Fest: Rezepte für Advent, Weihnachten und Silvester</li><li>Fingerfood vegan & vollwertig</li></ul>im Gesamtwert von 25,75 €",
         winner: "K0478",
       },
       {
@@ -277,42 +283,50 @@ const doors = [
       },
       {
         sponsor: "Mosterei Franz Decker, Weil der Stadt",
-        text: "1 Gutschein für 3 Pack Weil der Städter Apfelsaft (5 Liter Bag in Box)",
+        text:
+          "1 Gutschein für 3 Pack Weil der Städter Apfelsaft (5 Liter Bag in Box)",
         winner: "K0231",
       },
       {
         sponsor: "Mosterei Franz Decker, Weil der Stadt",
-        text: "1 Gutschein für 3 Pack Weil der Städter Apfelsaft (5 Liter Bag in Box)",
+        text:
+          "1 Gutschein für 3 Pack Weil der Städter Apfelsaft (5 Liter Bag in Box)",
         winner: "K1855",
       },
       {
         sponsor: "Mosterei Franz Decker, Weil der Stadt",
-        text: "1 Gutschein für 3 Pack Weil der Städter Apfelsaft (5 Liter Bag in Box)",
+        text:
+          "1 Gutschein für 3 Pack Weil der Städter Apfelsaft (5 Liter Bag in Box)",
         winner: "K1134",
       },
       {
         sponsor: "Mosterei Franz Decker, Weil der Stadt",
-        text: "1 Gutschein für 3 Pack Weil der Städter Apfelsaft (5 Liter Bag in Box)",
+        text:
+          "1 Gutschein für 3 Pack Weil der Städter Apfelsaft (5 Liter Bag in Box)",
         winner: "K1115",
       },
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text: "2 Kochbücher <ul><li>Pizza originale</li><li>Mozzarella originale</li></ul> im Gesamtwert von 34,95 €",
+        text:
+          "2 Kochbücher <ul><li>Pizza originale</li><li>Mozzarella originale</li></ul> im Gesamtwert von 34,95 €",
         winner: "K0217",
       },
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text: "2 Bücher<ul><li>Meze: Orientalische Gaumengeschenke</li><li>Santorin: Rezepte & Bilder aus der Ägäis</li></ul>im Gesamtwert von 31,95 €",
+        text:
+          "2 Bücher<ul><li>Meze: Orientalische Gaumengeschenke</li><li>Santorin: Rezepte & Bilder aus der Ägäis</li></ul>im Gesamtwert von 31,95 €",
         winner: "K1712",
       },
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text: "2 Bücher<ul><li>Das Männerkochbuch</li><li>Das Männerkochbuch – Crossover</li></ul>Im Gesamtwert von 41,90  €",
+        text:
+          "2 Bücher<ul><li>Das Männerkochbuch</li><li>Das Männerkochbuch – Crossover</li></ul>Im Gesamtwert von 41,90  €",
         winner: "K0199",
       },
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text: "2 Bücher<ul><li>Die Sprache der Küche</li><li>Der kleine Restaurantkritiker, Notizbuch</li></ul>Im Gesamtwert von 34,90  €",
+        text:
+          "2 Bücher<ul><li>Die Sprache der Küche</li><li>Der kleine Restaurantkritiker, Notizbuch</li></ul>Im Gesamtwert von 34,90  €",
         winner: "K0952",
       },
       {
@@ -505,7 +519,54 @@ const doors = [
     row: 3,
     col: 2,
     number: "17",
-    prices: [],
+    prices: [
+      {
+        sponsor: "BOPPEL MEDICAL ENGINEERING-Schlafdiagnose, Weil der Stadt",
+        text:
+          "1 Gutschein für eine ambulante Schlafuntersuchung Exklusiv inkl. EEG im Wert von 99,- €",
+        winner: "K0050",
+      },
+      {
+        sponsor: "FIT AVENUE, Weil der Stadt",
+        text: "1 Gutschein für einen Trainingsmonat im Wert von 100,- €",
+        winner: "K0474",
+      },
+      {
+        sponsor: "Metzgerei Heinkele und Partyservice, Weil der Stadt",
+        text: "1 Einkaufsgutschein im Wert von 20,- €",
+        winner: "K0923",
+      },
+      {
+        sponsor: "Metzgerei Heinkele und Partyservice, Weil der Stadt",
+        text: "1 Einkaufsgutschein im Wert von 20,- €",
+        winner: "K1535",
+      },
+      {
+        sponsor: "Radhaus Weber, Weil der Stadt",
+        text: "1 Einkaufsgutschein im Wert von 20,- €",
+        winner: "K0592",
+      },
+      {
+        sponsor: "Radhaus Weber, Weil der Stadt",
+        text: "1 Einkaufsgutschein im Wert von 20,- €",
+        winner: "K0159",
+      },
+      {
+        sponsor: "Radhaus Weber, Weil der Stadt",
+        text: "1 Einkaufsgutschein im Wert von 20,- €",
+        winner: "K0209",
+      },
+      {
+        sponsor: "Radhaus Weber, Weil der Stadt",
+        text: "1 Einkaufsgutschein im Wert von 20,- €",
+        winner: "K0617",
+      },
+      {
+        sponsor: "Radhaus Weber, Weil der Stadt",
+        text: "1 Einkaufsgutschein im Wert von 20,- €",
+        winner: "K0386",
+      },
+    ],
   },
   {
     row: 3,
@@ -514,22 +575,26 @@ const doors = [
     prices: [
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text: "Bodenseelust: Eine kulinarische Reise mit Rezepten der Region im Wert von 32,- €",
+        text:
+          "Bodenseelust: Eine kulinarische Reise mit Rezepten der Region im Wert von 32,- €",
         winner: "K0396",
       },
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text: "2 Bücher<ul><li>Die 12 wichtigsten essbaren Wildpflanzen: Bestimmen, sammeln und zubereiten</li><li>Die 12 besten essbaren Pionierpflanzen: Bestimmen, sammeln und zubereiten</li></ul> inkl. einem Sammelbeutel im Wert von 24,80 €",
+        text:
+          "2 Bücher<ul><li>Die 12 wichtigsten essbaren Wildpflanzen: Bestimmen, sammeln und zubereiten</li><li>Die 12 besten essbaren Pionierpflanzen: Bestimmen, sammeln und zubereiten</li></ul> inkl. einem Sammelbeutel im Wert von 24,80 €",
         winner: "K1404",
       },
       {
         sponsor: "SZ/BZ - Sindelfinger Zeitung/Böblinger Zeitung",
-        text: "1 Gutschein für ein Halbjahres-Abo für das  SZ/BZ-E-Paper im Wert von 137,- €",
+        text:
+          "1 Gutschein für ein Halbjahres-Abo für das  SZ/BZ-E-Paper im Wert von 137,- €",
         winner: "K1437",
       },
       {
         sponsor: "SZ/BZ - Sindelfinger Zeitung/Böblinger Zeitung",
-        text: "1 Gutschein für ein Halbjahres-Abo für das  SZ/BZ-E-Paper im Wert von 137,- €",
+        text:
+          "1 Gutschein für ein Halbjahres-Abo für das  SZ/BZ-E-Paper im Wert von 137,- €",
         winner: "K0128",
       },
     ],
@@ -547,12 +612,14 @@ const doors = [
     prices: [
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text: "2 Bücher<ul><li>Trüffeltang, Ananas-Ameisen und die Fässer des Herrn Takahashi</li><li>Der kleine Restaurantkritiker, Notizbuch</li></ul> im Gesamtwert von 35,- €",
+        text:
+          "2 Bücher<ul><li>Trüffeltang, Ananas-Ameisen und die Fässer des Herrn Takahashi</li><li>Der kleine Restaurantkritiker, Notizbuch</li></ul> im Gesamtwert von 35,- €",
         winner: "K0789",
       },
       {
         sponsor: "HÄDECKE VERLAG, Weil der Stadt",
-        text: "2 Kochbücher<ul><li>Brotaufstriche vegan & vollwertig</li><li>Suppen, Aufläufe und Eintöpfe vegan & vollwertig</li></ul> im Gesamtwert von 25,60 €",
+        text:
+          "2 Kochbücher<ul><li>Brotaufstriche vegan & vollwertig</li><li>Suppen, Aufläufe und Eintöpfe vegan & vollwertig</li></ul> im Gesamtwert von 25,60 €",
         winner: "K0031",
       },
       {
@@ -624,7 +691,8 @@ const doors = [
       },
       {
         sponsor: "INNENARCHITEKTIN JACQUELINE MÖRK, Weil der Stadt",
-        text: "1 Gutschein für eine Einrichtungberatung oder Home Staging im Wert von 700,-Euro",
+        text:
+          "1 Gutschein für eine Einrichtungberatung oder Home Staging im Wert von 700,-Euro",
         winner: "K1307",
       },
     ],
